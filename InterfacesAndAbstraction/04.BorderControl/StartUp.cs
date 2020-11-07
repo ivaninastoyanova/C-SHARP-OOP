2012db83e19c81036566159e823438a67e78b2d7
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2020-11-08 00:13:35 +0200

    Reordering

---
+++ InterfacesAndAbstraction/04.BorderControl/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using _04.BorderControl.Interfaces;
+using _04.BorderControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IId> creatures = new List<IId>();
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] splitted = command.Split().ToArray();
+                if (splitted.Length == 2)
+                {
+                    string model = splitted[0];
+                    string id = splitted[1];
+                    IId robot = new Robot(model, id);
+                    creatures.Add(robot);
+                }
+                else if (splitted.Length == 3)
+                {
+                    string name = splitted[0];
+                    int age = int.Parse(splitted[1]);
+                    string id = splitted[2];
+                    IId citizen = new Citizen(name, age, id);
+                    creatures.Add(citizen);
+                }
+            }
+            string fakeIdLastDigits = Console.ReadLine();
+            
+            creatures.Where(c => c.Id.EndsWith(fakeIdLastDigits))
+           .Select(c => c.Id)
+           .ToList()
+           .ForEach(Console.WriteLine);
+        }
+    }
+}
