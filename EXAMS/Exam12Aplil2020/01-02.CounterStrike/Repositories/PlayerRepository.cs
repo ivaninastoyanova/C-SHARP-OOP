f0a74fa8acfa9da5e981e6ec9f6675757bd3c2db
ivaninastoyanova <72800115+ivaninastoyanova@users.noreply.github.com>
2020-12-08 19:34:48 +0200

    Reordering

---
+++ EXAMS/Exam12Aplil2020/01-02.CounterStrike/Repositories/PlayerRepository.cs
@@ -0,0 +1,46 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Models.Players.Contracts;
+using CounterStrike.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CounterStrike.Repositories
+{
+    public class PlayerRepository : IRepository<IPlayer>
+    {
+        private List<IPlayer> models;
+        public PlayerRepository()
+        {
+            models = new List<IPlayer>();
+        }
+        public IReadOnlyCollection<IPlayer> Models
+        {
+            get
+            {
+                return this.models.AsReadOnly();
+            }
+        }
+
+        public void Add(IPlayer model)
+        {
+
+            if (model == null)
+            {
+                throw new ArgumentException("Cannot add null in Player Repository");
+            }
+            models.Add(model);
+        }
+
+        public IPlayer FindByName(string name)
+        {
+            return models.FirstOrDefault(m => m.Username == name);
+        }
+
+        public bool Remove(IPlayer model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
